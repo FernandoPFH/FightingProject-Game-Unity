2e2e710d954b0fb428c84227d637231c6ace5cd6
FernandoPFH <51247535+FernandoPFH@users.noreply.github.com>
2024-06-24 00:14:18 -0300

    Feat: Basic Moviment

---
+++ Assets/Scripts/PlayerMoviment.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PlayerMoviment : MonoBehaviour
+{
+    [Header("Horizontal Moviment")]
+    [SerializeField] float horizontalVelocity = 10f;
+    [SerializeField] float backwardsDecay = 0.8f;
+
+    [Header("Jump")]
+    [SerializeField] float verticalForce = 1f;
+    [SerializeField] float groundCheckDistance = 1.3f;
+    [SerializeField] LayerMask groundLayer;
+
+    [Header("Debug")]
+    [SerializeField] bool debug = false;
+
+    bool _isGrounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _isGrounded = Physics.Raycast(transform.position, transform.up * -1, out RaycastHit hitInfo, groundCheckDistance, groundLayer);
+
+        if (debug) Debug.DrawLine(transform.position, transform.position + transform.up * -1 * groundCheckDistance, Color.yellow);
+
+        Vector3 moviment = Vector3.right * HorizontalMoviment();
+
+        transform.Translate(moviment * Time.deltaTime);
+
+        GetComponent<Rigidbody>().AddForce(Vector3.up * VerticalMoviment(), ForceMode.Impulse);
+    }
+
+    float HorizontalMoviment()
+    {
+        float horizontalMoviment = 0f;
+
+        if (Input.GetKey(KeyCode.D))
+            horizontalMoviment += horizontalVelocity;
+        if (Input.GetKey(KeyCode.A))
+            horizontalMoviment -= horizontalVelocity * backwardsDecay;
+
+        return horizontalMoviment;
+    }
+
+    float VerticalMoviment()
+    {
+        float verticalMoviment = 0f;
+
+        if (Input.GetKeyDown(KeyCode.W) && _isGrounded)
+            verticalMoviment = verticalForce;
+
+        return verticalMoviment;
+    }
+}
